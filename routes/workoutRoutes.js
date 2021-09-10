f875ac1a4c6866d57f68f49826f0c9f536b4a744
Rodrick Sims <rsimstransport@gmail.com>
2021-09-10 14:44:24 -0400

    route setup

---
+++ routes/workoutRoutes.js
@@ -0,0 +1,90 @@
+const router = require('express').Router();
+
+
+//requiring Workout model 
+const Workout = require('../models/Workout');
+
+//Getting last workout
+router.get('/api/workouts', async (req, res) => {
+    try {
+        const lastWorkout = await Workout.aggregate([
+            {
+                $addFields: {
+                    totalDuration: {$sum: "$exercises.duration"}
+                }
+            },
+            /*{
+                $addFields:{
+                    name: ["$exercises.name"]
+                }
+            }*/
+        ]).sort({ date: -1 });
+        console.log(lastWorkout);
+        res.status(200).json(lastWorkout);
+    } catch (e) {
+        res.status(500).json(e);
+    }
+});
+
+
+//Getting all workouts in range from Workout
+router.get('/api/workouts/range', async (req, res) => {
+    try {
+        const allWorkouts = await Workout.aggregate([
+            {
+                $addFields: {
+                    totalDuration: {
+                        $sum: "$exercises.duration"
+                    }
+                }
+            }
+        ]).sort({ day: -1 }).limit(7);
+        res.status(200).json(allWorkouts);
+    } catch (e) {
+        res.status(500).json(e);
+    }
+})
+
+//Creating a new workout
+router.post('/api/workouts', async (req, res) => {
+    try {
+        const newWorkout = await Workout.create(req.body);
+        newWorkout.save();
+
+        res.status(200).json(newWorkout);
+    } catch (e) {
+        res.status(500).json(e);
+    }
+});
+
+//Updating a workout by adding exercise by id
+router.put('/api/workouts/:id', async (req, res) => {
+    try {
+        const addExercise = await Workout.findByIdAndUpdate(req.params.id, {
+            $push: {
+                exercises: req.body
+            },
+        }, {
+            new: true,
+        },
+        );
+
+        res.status(200).json(addExercise);
+    } catch (e) {
+        res.status(500).json(e);
+    }
+});
+
+
+//Deleting workouts
+router.delete("/api/workouts", ({ body }, res) => {
+    Workout.findByIdAndDelete(body.id)
+        .then(() => {
+            res.json(true);
+        })
+        .catch((err) => {
+            res.status(400).json(err);
+        });
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const mongoose = require ('mongoose');
+const logger = require("morgan");
+const path = require('path');
+require('dotenv').config();
+
+//getting routes
+//const workout = require('./routes/workoutRoutes');
+
+//requiring schemas
+//const {Workout}= require ('./models');
+
+//setting up Port
+const PORT= process.env.PORT || 3001;
+
+//different in production // if db not created yet mongoose will create db automatically
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    //giving me errors to connect...
+    useCreateIndex: true,
+    useFindAndModify: false,
+    
+})
+    .then(() => {
+        console.log('Connected to mongodb successfully');
+    })
+
+    .catch(error => console.log(error));
+
+
+//setting up express as a function
+const app = express();
+
+
+//logger middleware (dev: color coded)
+app.use(logger("dev"));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+//middlewares to get values from req.body
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//requiring routes
+//app.use(workout);
+app.use(require('./routes/workoutRoutes.js'));
+app.use(require('./routes/htmlRoutes.js'));
+
+app.listen(PORT, ()=>{
+    console.log(`Listening on http://localhost:${PORT}`);
+} )
