2e4b410885b69516b7b9997c1b0d2757a12db04b
Rodrick Sims <rsimstransport@gmail.com>
2021-09-21 23:55:53 -0400

    updated routes

--- routes/api/homeRoutes.js
+++ routes/api/homeRoutes.js
@@ -1,21 +1,21 @@
 const router = require("express").Router();
 const path = require("path");
 
-//index
-router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
-})
+
 
 
 //stats
 router.get('/stats', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/stats.html'));
+    res.sendFile(path.join(__dirname, '../../public/stats.html'));
 })
 
 
 //exercise
 router.get('/exercise', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/exercise.html'));
+    res.sendFile(path.join(__dirname, '../../public/exercise.html'));
 })
-
+//index
+router.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/index.html'));
+})
 module.exports = router;
